f10051831a0191418a2cef92f142f909a2fecead
Aleksandr Tugolukov <billgadov3k@gmail.com>
2022-09-07 23:03:19 +0300

    поигрался с props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
+import MainComponent from "./components/main-component";
 
 function App() {
+	const title = "Hello React App";
+	const title2 = "Monkey";
+
 	return (
 		<div className="App">
-			<h1>Hello React!!!</h1>
+			<h1>{title}</h1>
 			<h2>this is my first react app</h2>
+			<MainComponent title={title2} />
 		</div>
 	);
 }
